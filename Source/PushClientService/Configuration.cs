860f081a4a787ac4f15de88de543a9979a9391ac
David Wood <david.p.wood@gmail.com>
2015-02-13 00:18:10 +0000

    Added secret support.

--- Source/PushClientService/Configuration.cs
+++ Source/PushClientService/Configuration.cs
@@ -6,12 +6,14 @@ namespace PushClientService
     {
         public static string PushProxyUrl { get; private set; }
         public static string CiUrl { get; private set; }
+        public static string Secret { get; private set; }
 
         static Configuration()
         {
             var appSettings = ConfigurationManager.AppSettings;
-            CiUrl = appSettings["CI_URL"];
             PushProxyUrl = appSettings["PUSH_PROXY_URL"];
+            CiUrl = appSettings["CI_URL"];
+            Secret = appSettings["SECRET"];
         }
     }
 }
--- Source/PushClientService/Server.cs
+++ Source/PushClientService/Server.cs
@@ -15,22 +15,26 @@ namespace PushClientService
             _socket.On(Socket.EVENT_CONNECT, () =>
             {
                 _log.InfoFormat("Connected to {0}.", Configuration.PushProxyUrl);
+
+                _socket.Emit("secret",
+                    data => _log.InfoFormat("Subscribed to secret: {0}", Configuration.Secret),
+                    Configuration.Secret);
+
                 _socket.On("PushEvent", data =>
                 {
-                    _log.Info("PushEvent received: " + data.ToString());
+                    _log.Info("PushEvent received");
+                    _log.Debug("Payload: " + data.ToString());
                     PushService.Push(data);
                 });
             });
-            _socket.On(Socket.EVENT_DISCONNECT, () =>
-            {
-                _log.InfoFormat("Disconnected from {0}.", Configuration.PushProxyUrl);
-            });
+            _socket.On(Socket.EVENT_DISCONNECT, () => _log.InfoFormat("Disconnected from {0}.", Configuration.PushProxyUrl));
             _log.Info("Service started.");
         }
 
         public void Stop()
         {
             _log.Info("Service stopping...");
+            _socket.Close();
             _socket.Disconnect();
             _log.Info("Service stopped.");
         }
